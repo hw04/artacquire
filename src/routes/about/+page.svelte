cf51ce2693c7a0a271d8862cc3a9af3025751ef9
hw04 <96802643+hw04@users.noreply.github.com>
2024-04-29 12:06:50 +0100

    Update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,12 @@
-<h1>About</h1>
+<h1 class="text-3xl font-bold text-center mt-6 mb-6">About</h1>
+
+<div class="flex justify-center m-3 mt-20">
+	<article class="prose text-center">
+		<p>This website displays artwork from the V&A and Art Institute of Chicago museums APIs.</p>
+		<p>
+			Users can enter general search queries and add artwork they like to their collection, which
+			persists even if the browser is closed.
+		</p>
+		<p>This website was built using SvelteKit.</p>
+	</article>
+</div>
