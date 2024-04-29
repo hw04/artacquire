a2e5c33f6fe45b83aec416d2109fd5b43f193ffb
hw04 <96802643+hw04@users.noreply.github.com>
2024-04-29 12:01:15 +0100

    Update home page and layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,54 @@
 <script>
+	import { base } from "$app/paths"
 	import { goto } from "$app/navigation"
 	import "../app.css"
 </script>
 
-<nav class="navbar">
-	<button on:click={() => goto("/")}>Home/Logo</button>
-	<button on:click={() => goto("/explore")}>Explore</button>
-	<button on:click={() => goto("/myexhibition")}>My Exhibition</button>
-	<button on:click={() => goto("/random", { invalidateAll: true })}>Random</button>
-	<button on:click={() => goto("/about")}>About</button>
-</nav>
+<div class="navbar bg-base-300">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h8m-8 6h16"
+					/></svg
+				>
+			</div>
+			<ul
+				tabindex="0"
+				class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+			>
+				<li><button on:click={() => goto(`${base}/explore`)}>Explore</button></li>
+				<li><button on:click={() => goto(`${base}/gallery`)}>Gallery</button></li>
+				<li>
+					<button on:click={() => goto(`${base}/random`, { invalidateAll: true })}>Random</button>
+				</li>
+				<li><button on:click={() => goto(`${base}/about`)}>About</button></li>
+			</ul>
+		</div>
+		<a class="btn btn-ghost text-xl" href="/">ArtAcquire</a>
+	</div>
+	<div class="navbar-end hidden lg:flex space-x-2">
+		<button class="btn btn-primary" on:click={() => goto(`${base}/explore`)}>Explore</button>
+		<button class="btn btn-primary" on:click={() => goto(`${base}/gallery`)}>Gallery</button>
+		<button class="btn btn-primary" on:click={() => goto(`${base}/random`, { invalidateAll: true })}
+			>Random</button
+		>
+		<button class="btn btn-primary" on:click={() => goto(`${base}/about`)}>About</button>
+	</div>
+</div>
 
 <slot />
 
-<style>
+<style lang="postcss">
 	.navbar {
 		display: flex;
 		justify-content: space-between;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Hello World!</h1>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Welcome to ArtAcquire</h1>
+			<p class="py-6">Please click one of the buttons above, or the button below to get started</p>
+			<a class="btn btn-primary" href="/explore">Get Started</a>
+		</div>
+	</div>
+</div>
